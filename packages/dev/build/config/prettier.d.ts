c44082469f90bb97fded596c554deb1541a4d1ad
Tyson Lupul <wavesrcool@icloud.com>
2022-09-15 15:19:21 -0500

    feat(dev): adds to config/ prettier configuration

---
+++ packages/dev/build/config/prettier.d.ts
@@ -0,0 +1,12 @@
+declare const _default: {
+    semi: boolean;
+    trailingComma: string;
+    singleQuote: boolean;
+    printWidth: number;
+    tabWidth: number;
+    useTabs: boolean;
+    arrowParens: string;
+    endOfLine: string;
+};
+export default _default;
+//# sourceMappingURL=prettier.d.ts.map
---
+++ packages/dev/src/config/prettier.ts
@@ -0,0 +1,10 @@
+export default {
+  semi: false,
+  trailingComma: "es5",
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+};
