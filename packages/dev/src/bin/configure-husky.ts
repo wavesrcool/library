9ac08b43fd2456562db7407beb8141a54527c2f6
Tyson Lupul <wavesrcool@icloud.com>
2022-09-21 13:57:42 -0500

    feat(dev): adds bin configure-husky

---
+++ packages/dev/src/bin/configure-husky.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+// Copyright 2020-2022 @wavesrcool/dev authors & contributors
+// SPDX-License-Identifier: MIT
+
+import { exec } from "child_process";
+import { mkdirSync, writeFileSync } from "fs";
+import { join } from "path";
+
+const data = `#!/usr/bin/env sh
+. "$(dirname -- "$0")/_/husky.sh"
+yarn lint-staged --allow-empty`;
+
+const main = () => {
+  const directory = join(process.cwd(), ".husky");
+  mkdirSync(directory, { recursive: true });
+  const filename = join(directory, "pre-commit");
+  writeFileSync(filename, data);
+  exec(`yarn husky install`);
+};
+
+main();
